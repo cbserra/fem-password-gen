fc51baf7fb3d4ef5d809fbc7d21a9765d0c9b075
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-08-31 21:04:16 -0700

    :lipstick: ran linters/formatters

--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
 import { twclsx } from '@/utils'
 
+import React from 'react'
+
 const RangeSlider = (params: {
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }) => {
-    const { handleChange } = params
-    const classNames = twclsx("password__gen-options--length flex items-center justify-center w-full")
+  const { handleChange } = params
+  const classNames = twclsx('password__gen-options--length flex items-center justify-center w-full')
   return (
     <div className={classNames}>
-      <div className="password-gen--options--length--input-wrapper flex flex-col w-full gap-y-[.8rem]">
-        <div className="password-gen--options--length--label-wrapper flex justify-between items-center">
-        <label htmlFor="char-length" className="">
-          Character Length
-        </label>
-          <span className="password__gen-options--length--value text-heading-md text-secondary-neon-green font-bold">12</span>
+      <div className='password-gen--options--length--input-wrapper flex flex-col w-full gap-y-[.8rem]'>
+        <div className='password-gen--options--length--label-wrapper flex justify-between items-center'>
+          <label htmlFor='char-length' className=''>
+            Character Length
+          </label>
+          <span className='password__gen-options--length--value text-heading-md text-secondary-neon-green font-bold'>
+            12
+          </span>
         </div>
-        
-        <input 
-          type="range" 
-          name="char-length" 
-          id="char-length" 
-          min="8" 
-          max="32" 
-          className={twclsx("password__gen-options--range focus:outline-none ")} 
-          onChange={handleChange}  
+
+        <input
+          type='range'
+          name='char-length'
+          id='char-length'
+          min='8'
+          max='32'
+          className={twclsx('password__gen-options--range focus:outline-none ')}
+          onChange={handleChange}
         />
-        
-        </div>
+      </div>
     </div>
   )
 }
--- src/utils/PasswordGenerator.ts
+++ src/utils/PasswordGenerator.ts
@@ -1,58 +1,67 @@
-import { GeneratePasswordOptions } from "@/types/Types"
+import { GeneratePasswordOptions } from '@/types/Types'
 
 enum DictionaryType {
-    Lowercase = "lowercase",
-    Uppercase = "uppercase",
-    Numbers = "numbers",
-    Symbols = "symbols",
+  Lowercase = 'lowercase',
+  Uppercase = 'uppercase',
+  Numbers = 'numbers',
+  Symbols = 'symbols'
 }
 
-const LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
-const UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const NUMBERS = "0123456789"
+const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+const UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const NUMBERS = '0123456789'
 const SYMBOLS = "!@#$%^&*()_+-=[]{};':,./<>?`~"
 
-
 export function generatePassword(options: GeneratePasswordOptions): string {
-    const { length, includeLowercase, includeUppercase, includeNumbers, includeSymbols } = options
-    const dictionary = buildDictionary(includeLowercase, includeUppercase, includeNumbers, includeSymbols)
-    const keys = Array.from(dictionary.keys())
+  const { length, includeLowercase, includeUppercase, includeNumbers, includeSymbols } = options
+  const dictionary = buildDictionary(
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  )
+  const keys = Array.from(dictionary.keys())
 
-    let password = ''
-    while (password.length < length) {
-        const shuffledKeys = keys.sort(() => 0.5 - Math.random())
+  let password = ''
+  while (password.length < length) {
+    const shuffledKeys = keys.sort(() => 0.5 - Math.random())
 
-        shuffledKeys.forEach(key => {
-            const dictionaryValue = dictionary.get(key)
-            if (dictionaryValue) {
-                password += dictionaryValue[Math.floor(Math.random() * dictionaryValue.length)]
-            }
-        })
-    }
-    
-    return password
+    shuffledKeys.forEach((key) => {
+      const dictionaryValue = dictionary.get(key)
+      if (dictionaryValue) {
+        password += dictionaryValue[Math.floor(Math.random() * dictionaryValue.length)]
+      }
+    })
+  }
+
+  return password
 }
 
-function buildDictionary(includeLowercase: boolean, includeUppercase: boolean, includeNumbers: boolean, includeSymbols: boolean): Map<DictionaryType, string[]> {
-    const dictionary = new Map<DictionaryType, string[]>()
+function buildDictionary(
+  includeLowercase: boolean,
+  includeUppercase: boolean,
+  includeNumbers: boolean,
+  includeSymbols: boolean
+): Map<DictionaryType, string[]> {
+  const dictionary = new Map<DictionaryType, string[]>()
 
-    if (includeLowercase) {
-        dictionary.set(DictionaryType.Lowercase, LOWERCASE_LETTERS.split(''))
-    }
+  if (includeLowercase) {
+    dictionary.set(DictionaryType.Lowercase, LOWERCASE_LETTERS.split(''))
+  }
 
-    if (includeUppercase) {
-        dictionary.set(DictionaryType.Uppercase, UPPERCASE_LETTERS.split(''))
-    }
+  if (includeUppercase) {
+    dictionary.set(DictionaryType.Uppercase, UPPERCASE_LETTERS.split(''))
+  }
 
-    if (includeNumbers) {
-        dictionary.set(DictionaryType.Numbers, NUMBERS.split(''))
-    }
+  if (includeNumbers) {
+    dictionary.set(DictionaryType.Numbers, NUMBERS.split(''))
+  }
 
-    if (includeSymbols) {
-        dictionary.set(DictionaryType.Symbols, SYMBOLS.split(''))
-    }
+  if (includeSymbols) {
+    dictionary.set(DictionaryType.Symbols, SYMBOLS.split(''))
+  }
 
-    return dictionary
+  return dictionary
 }
 
 // module.exports = { generatePassword }
